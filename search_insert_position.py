e0d89131d65d0c4734af94a784d2bafde7139b70
Yongzhi Pan <panyongzhi@gmail.com>
2014-04-15 18:03:57 +0800

    http://oj.leetcode.com/problems/search-insert-position/

---
+++ search_insert_position.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+"""
+http://oj.leetcode.com/problems/search-insert-position/
+
+Given a sorted array and a target value, return the index if the target is
+found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+Since Apr-15-2014 18:02
+"""
+
+class Solution:
+    # @param A, a list of integers
+    # @param target, an integer to be inserted
+    # @return integer
+    def searchInsert(self, A, target):
+        pos = 0
+        for i in range(len(A)):
+            if target == A[i]:
+                return i
+            elif target < A[i]:
+                return i
+        else:
+            return len(A)
