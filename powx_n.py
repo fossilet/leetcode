33bf03d588da438131a9077da61c90abb3876a23
Yongzhi Pan <panyongzhi@gmail.com>
2014-08-30 00:37:04 +0800

    Optimize to O(logN) time.  fix #1

--- powx_n.py
+++ powx_n.py
@@ -16,5 +16,17 @@ class Solution:
     def pow(self, x, n):
         if n == 0:
             return 1
+        elif n == 1:
+            return x
+        elif n % 2 == 0:
+            return pow(x, n / 2) * pow(x, n / 2)
         else:
             return x * pow(x, n - 1)
+
+if __name__ == '__main__':
+    assert pow(2, 0) == 1
+    assert pow(0.2, 1) == 0.2
+    assert pow(-2, 2) == 4
+    assert pow(2, 3) == 8
+    assert pow(2, 6) == 64
+    assert pow(2, 12) == 4096
