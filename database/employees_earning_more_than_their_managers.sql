eed46af32d8bd3549cc3964d286ea2561e8f6184
Yongzhi Pan <panyongzhi@gmail.com>
2015-03-02 18:18:25 +0800

    Reformat SQL.

--- database/employees_earning_more_than_their_managers.sql
+++ database/employees_earning_more_than_their_managers.sql
@@ -1,2 +1,6 @@
 -- Since Mon Mar  2 18:14:08 CST 2015
-select Name as Employee from Employee as e1 where ManagerId is not NULL and Salary > (select Salary from Employee where Id = e1.ManagerId) 
+SELECT Name AS Employee
+FROM Employee AS e1
+WHERE ManagerId IS NOT NULL AND Salary > (SELECT Salary
+                                          FROM Employee
+                                          WHERE Id = e1.ManagerId)
